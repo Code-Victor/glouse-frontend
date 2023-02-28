cbd5e22b90103e66f7bbfbf92370621ce82b2c26
Your name <oluwaborihamzat@gmail.com>
2023-02-28 19:00:06 +0100

    feat: created a select

---
+++ src/components/inc/Select.tsx
@@ -0,0 +1,84 @@
+import * as RadixSelect from "@radix-ui/react-select";
+import { css, styled } from "stitches.config";
+import { ChevronDownIcon } from "../icons";
+import * as React from "react";
+
+export default function Select({
+  data,
+}: {
+  data: { id: string; name: string }[];
+}) {
+  const [selected, setSelected] = React.useState(data[0].name);
+
+  return (
+    <RadixSelectRoot
+      value={selected}
+      onValueChange={(value) => setSelected(value)}
+    >
+      <RadixSelect.Trigger
+        className={css({
+          border: "none",
+          bg: "$primary",
+          px: "$4",
+          py: "$2",
+          color: "$white",
+          br: "$pill",
+          display: "flex",
+          gap: "$2",
+        })()}
+      >
+        <RadixSelect.Value placeholder="Select mode..." />
+        <RadixSelect.Icon className="SelectIcon">
+          <ChevronDownIcon
+            css={{
+              color: "$white",
+            }}
+          />
+        </RadixSelect.Icon>
+      </RadixSelect.Trigger>
+      <RadixSelect.Portal>
+        <RadixSelect.Content>
+          <RadixSelect.Viewport
+            className={css({
+              // border: "none",
+              bg: "$white",
+              py: "$2",
+              color: "$secondary",
+              br: "$4",
+              px: "$2",
+              boxShadow:
+                "0 0 0 1px gainsboro, 0 4px 14px -3px rgba(0, 0, 0, 0.3)",
+            })()}
+          >
+            {data.map((item) => (
+              <RadixSelect.Item
+                value={item.name}
+                key={item.id}
+                className={css({
+                  transition: "all 100ms ease-out",
+                  px: "$1",
+                  py: "$1",
+                  fontWeight: 500,
+                  "&:hover,&:focus": {
+                    bg: "$primary",
+                    color: "$white",
+                    br: "$1",
+                  },
+                })()}
+              >
+                <RadixSelect.ItemText>{item.name}</RadixSelect.ItemText>
+              </RadixSelect.Item>
+            ))}
+          </RadixSelect.Viewport>
+        </RadixSelect.Content>
+      </RadixSelect.Portal>
+    </RadixSelectRoot>
+  );
+}
+
+const RadixSelectRoot = styled(RadixSelect.Root, {
+  display: "inline-flex",
+});
+const RadixSelectItem = styled(RadixSelect.Item, {});
+
+Select.Item = RadixSelectItem;
